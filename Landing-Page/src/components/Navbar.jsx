77e9e24009fbb1cba4782baf97aec952cab69529
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-09-15 20:56:24 +0500

    Landing Page

---
+++ Landing-Page/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import logo from "../assets/logo-1.avif"
+import {navItems} from "../constants/index.jsx"
+const Navbar = () => {
+
+  return (
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+   
+     <div className="container px-4 mx-auto relative text-sm">
+       <div className="flex-justify-center items-center">
+        <div className="flex items-center flex-shrink-0">
+           <img className='w-10 h-18 mr-2' src={logo} alt="logo" /> 
+           <span className="text-xl tracking-tight">FreedoM</span>
+        </div>
+        <ul className="hidden lg:flex ml-14 space-x-12">
+        {navItems.mao((item, index) =>{
+            
+        })}
+        </ul>
+       </div>
+     </div>
+    </nav>
+  )
+}
+
+export default Navbar
