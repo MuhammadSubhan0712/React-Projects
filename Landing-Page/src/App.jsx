50b01eaa96150e32249b88e8bf2a079b28c7f1ca
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-09-16 01:45:03 +0500

    Landing Page

--- Landing-Page/src/App.jsx
+++ Landing-Page/src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
 import FeatureSection from './components/FeatureSection'
+import  Workflow from './components/Workflow'
 
 const App = () => {
   return (
@@ -13,6 +14,8 @@ const App = () => {
     </div>
 
     <FeatureSection />
+
+    <Workflow />
 
     </>
   )
