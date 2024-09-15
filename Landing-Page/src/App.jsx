3a6aebd52c0628964a39a3333f67470fa23a48e6
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-09-16 00:03:19 +0500

    Landing Page

--- Landing-Page/src/App.jsx
+++ Landing-Page/src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
 import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import FeatureSection from './components/FeatureSection'
 
 const App = () => {
   return (
     
     <>
     <Navbar/>
+
+    <div className="max-w-7x1 mx-auto pt-20 px-6">
+      <HeroSection />
+    </div>
+
+    <FeatureSection />
+
     </>
   )
 }
