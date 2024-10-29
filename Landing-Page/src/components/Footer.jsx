7db820bae0d46ac9d020625c45c1285dfacca512
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-10-30 02:38:53 +0500

    Working..

--- Landing-Page/src/components/Footer.jsx
+++ Landing-Page/src/components/Footer.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { resourcesLinks , platformLinks , communityLinks } from "../constants"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="mt-20 border-t py-10 border-neutral-700">
+<div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+
+<div>
+    <h3 className="text-md font-semibold mb-4"> Resources
+    
+    <ul className="space-y-2">
+        {resourcesLinks.map((link , index) => {
+            <li key={index}>
+                <a href={link.href} className="text-neutral-300 hover:text-white">{link.text}</a>
+            </li>
+        })}
+    </ul>
+    </h3>
+</div>
+</div>
+
+    </footer>
+    
   )
 }
 
