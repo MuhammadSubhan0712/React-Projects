4d6e14377189c6b9e0a5e4d6152076c3dde00fb4
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-11-17 23:56:25 +0500

    Working..

--- Landing-Page/src/components/Footer.jsx
+++ Landing-Page/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { resourcesLinks, platformLinks, communityLinks } from "../constants";
 const Footer = () => {
   return (
     <footer className="mt-20 border-t py-10 border-neutral-700">
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+       <div className="container mx-auto flex flex-wrap justify-around gap-5">
         <div>
           <h3 className="text-md font-semibold mb-4">
             {" "}
