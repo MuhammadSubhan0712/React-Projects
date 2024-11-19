c9bfa24453d878482937bc1c088b07a5237da4d4
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-11-19 23:57:01 +0500

    Working....

--- Coffee-Shop/index.js
+++ Coffee-Shop/index.js
@@ -1,15 +1,29 @@
-const navbar = document.querySelector(".navbar")
+const navbar = document.querySelector(".navbar");
 const menubtn = document.querySelector("#menu-btn");
 const section = document.querySelector("section");
 const navLinks = document.querySelector("header .navbar a");
 
+menubtn.onClick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
 
-
-
-
-menubtn.onClick = () =>{
-navbar.classList.toggle('active')
-}
-window.onscroll = () =>{
-    navbar.classList.remove 
-}
+  section.forEach((sec) => {
+    let top = window.scrollBy;
+    let height = sec.offsetHeight;
+    let offset = (sec.offsetTop = 150);
+    let id = sec.getAttribute("id");
+    if (top >= offset && top < offset + height) {
+      navLinks.foreach((links) => {
+        links.classList.remove("active");
+        document.querySelector("header .navbar a[href*=" + id + "]");
+      });
+    }
+  });
+  if (window.scrollY > 50) {
+    document.querySelector("#scroll-top").classList.add("navbar");
+  } else {
+    document.querySelector("#scroll-top").classList.remove("active");
+  }
+};
