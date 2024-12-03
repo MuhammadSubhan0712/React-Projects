d84fb0f921e05050a92ce6d982002b69595571ac
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-12-03 13:34:29 +0500

    Working....

--- netflix-clone/src/components/Navbar/Navbar.jsx
+++ netflix-clone/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import "./Navbar.css"
-import logo from  "../../assets/logo.png"
-import search_icon from  "../../assets/search_icon.svg"
-import bell_icon from  "../../assets/bell_icon.svg"
-import profile_img from  "../../assets/profile_img.png"
-import profile_img from  "../../assets/profile_img.png"
-
+import logo from '../../assets/logo.png';
+import search_icon from '../../assets/search_icon.svg';
+import bell_icon from '../../assets/bell_icon.svg';
+import caret_icon from '../../assets/profile_icon.svg';
+import profile_img from '../../assets/profile_img.png';
 
 const Navbar = () => {
   return (
@@ -26,9 +25,11 @@ const Navbar = () => {
     <p>Children</p>
     <img className='icons' src={bell_icon} alt="bell icon" />
     <div className="navbar-profile">
-    <img className='icons' src={bell_icon} alt="bell icon" />
-    <img className='icons' src={bell_icon} alt="bell icon" />
-
+    <img className='profile' src={profile_img} alt="profile image" />
+    <img className='caret' src={caret_icon} alt="caret icon" />
+    <div className="dropdown">
+    <p>Sign Out Netflix</p>
+    </div>
     </div>
     </div>
     </div>
